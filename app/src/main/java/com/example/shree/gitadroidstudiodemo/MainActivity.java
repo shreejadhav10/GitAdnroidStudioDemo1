8328a6e42320fd6ddf45c99ec0683577eb4d9415
Shripad Jadhav <shreejadhav10@gmail.com>
2018-04-19 18:25:17 +0530

    added new toasts

--- app/src/main/java/com/example/shree/gitadroidstudiodemo/MainActivity.java
+++ app/src/main/java/com/example/shree/gitadroidstudiodemo/MainActivity.java
@@ -21,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 }
